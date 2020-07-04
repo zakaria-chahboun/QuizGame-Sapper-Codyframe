0754bff7ed0460a92b0c676c83ddfdfd8723b5db
zakaria chahboun <zaki@localhost.localdomain>
2020-07-04 14:46:04 +0100

    update layout choices and stpes components

--- src/components/test/Choices.svelte
+++ src/components/test/Choices.svelte
@@ -3,21 +3,15 @@
   import { types } from "./types.js";
 
   export let isMultiple = false;
+  export let data = {
+    data1: { type: types.current, answer: "Hola!" },
+    data2: { type: types.uncorrect, answer: "Hola!" },
+    data3: { type: types.correct, answer: "Hola!" },
+    data4: { type: types.uncorrect, answer: "Hola!" }
+  };
 </script>
 
-<Choice
-  type={types.current}
-  answer="hala"
-  id="radio1"
-  group={isMultiple ? '' : 'my-group'} />
-<Choice
-  type={types.correct}
-  answer="hala"
-  id="radio2"
-  group={isMultiple ? '' : 'my-group'} />
-<Choice
-  type={types.uncorrect}
-  answer="hala"
-  id="radio3"
-  group={isMultiple ? '' : 'my-group'} />
-<Choice group={isMultiple ? '' : 'my-group'} />
+<Choice {...data.data1} id="radio1" group={isMultiple ? '' : 'my-group'} />
+<Choice {...data.data2} id="radio2" group={isMultiple ? '' : 'my-group'} />
+<Choice {...data.data3} id="radio3" group={isMultiple ? '' : 'my-group'} />
+<Choice {...data.data4} group={isMultiple ? '' : 'my-group'} />
