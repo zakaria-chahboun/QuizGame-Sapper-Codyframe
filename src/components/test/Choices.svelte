d63050761f15cce81e456f01dc9b399c3ee13188
zakaria chahboun <zaki@localhost.localdomain>
2020-07-04 18:43:55 +0100

    update choices events

--- src/components/test/Choices.svelte
+++ src/components/test/Choices.svelte
@@ -1,17 +1,51 @@
 <script>
-  import Choice from "./Choice.svelte";
   import { types } from "./types.js";
 
+  // export let type = "";
+  // export let answer = "Hola!";
+  // export let disabled = false;
   export let isMultiple = false;
-  export let data = {
-    data1: { type: types.current, answer: "Hola!" },
-    data2: { type: types.uncorrect, answer: "Hola!" },
-    data3: { type: types.correct, answer: "Hola!" },
-    data4: { type: types.uncorrect, answer: "Hola!" }
-  };
+
+  export let data = [
+    { type: types.correct, answer: "Hola1 !", disabled: false },
+    { type: types.uncorrect, answer: "Hola2 !", disabled: false },
+    { type: types.current, answer: "Hola3 !", disabled: false },
+    { type: types.correct, answer: "Hola4 !", disabled: false }
+  ];
+
+  export let scoopsRadio = 0; // for getting the chosen radio "answer" (in single choice)
+  export let scoopCheck = []; // for getting the chosen chekbox "answer" (in multiple choices)
+
+  $: console.log(scoopCheck);
 </script>
 
-<Choice {...data.data1} id="radio1" group={isMultiple ? '' : 'my-group'} />
-<Choice {...data.data2} id="radio2" group={isMultiple ? '' : 'my-group'} />
-<Choice {...data.data3} id="radio3" group={isMultiple ? '' : 'my-group'} />
-<Choice {...data.data4} group={isMultiple ? '' : 'my-group'} />
+<!-- For Single Answer: Radio Button -->
+{#if !isMultiple}
+  {#each data as { type, answer, disabled }, i}
+    <li>
+      <input
+        class="radio radio--bg {type}"
+        type="radio"
+        id="radio{i + 1}"
+        name={isMultiple ? '' : 'my-group'}
+        {disabled}
+        value={i + 1}
+        bind:group={scoopsRadio} />
+      <label for="radio{i + 1}">{answer}</label>
+    </li>
+  {/each}
+  <!-- For Multiple Answers: CheckBox -->
+{:else}
+  {#each data as { type, answer, disabled }, i}
+    <li>
+      <input
+        class="checkbox radio--bg {type}"
+        type="checkbox"
+        id="check{i + 1}"
+        {disabled}
+        value={i + 1}
+        bind:group={scoopCheck} />
+      <label for="check{i + 1}">{answer}</label>
+    </li>
+  {/each}
+{/if}
--- src/routes/test/index.svelte
+++ src/routes/test/index.svelte
@@ -1,10 +1,10 @@
 <script>
-  import Choice from "../../components/test/Choice.svelte";
   import Step from "../../components/test/Step.svelte";
   import Choices from "../../components/test/Choices.svelte";
   import MyLayout from "./_myLayout.svelte";
   import { types } from "../../components/test/types.js";
 
+  // steps data
   let table = [
     { type: types.current, url: "google.com" },
     { type: types.correct, url: "google.com" },
@@ -25,12 +25,42 @@
     { type: "", url: "#" },
     { type: "", url: "#" }
   ];
+
+  let scoopsRadio = 0; // for getting the chosen radio "answer" (in single choice)
+  //let scoopCheck = []; // for getting the chosen chekbox "answer" (in multiple choices)
+
+  let isMultiple = !false; // if multiple answers or a single one
+  $: console.log(scoopsRadio);
+
+  // Choices Data: "format"
+  let data = [
+    {
+      type: types.correct,
+      answer: "Hi! How are you? are you fine? really? 01",
+      disabled: false
+    },
+    {
+      type: types.correct,
+      answer: "Hi! How are you? are you fine? really? 02",
+      disabled: !false
+    },
+    {
+      type: types.current,
+      answer: "Hi! How are you? are you fine? really? 03",
+      disabled: !false
+    },
+    {
+      type: types.correct,
+      answer: "Hi! How are you? are you fine? really? 04",
+      disabled: false
+    }
+  ];
 </script>
 
 <MyLayout>
 
-  <!-- Choices Data as: x4 {data1:{type:...,answer:....},data2:{type:...,answer:....}}-->
-  <Choices />
+  <!-- Choices Data as: x4 let data=[{type:...,answer:...,disabled:...},{type:...,answer:...,disabled:...}]-->
+  <Choices {isMultiple} bind:scoopsRadio {data} />
 
   <!-- Steps Data -->
   <span slot="steps">
