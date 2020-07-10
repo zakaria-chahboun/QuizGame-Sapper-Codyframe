fe36359473b6832f07d1226a5c02baf524e9c30c
zakaria chahboun <zaki@localhost.localdomain>
2020-07-10 14:12:58 +0100

    update firebase conf

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,13 @@
 import * as firebase from "firebase-admin";
 import config from "./private/firebase-config.json";
+import dUrl from "./private/firebase-databse-url.json";
 
 // You have to put your firebase configurations in 'src/private/firebase-config.json' file, check readme
+// You have to put your firebase database url in 'src/private/firebase-database-url.json' file, check readme
 
 firebase.initializeApp({
     credential: firebase.credential.cert(config),
-    databaseURL: "https://quiz-e9e49.firebaseio.com"
+    databaseURL: dUrl.databaseURL
 });
 
 export const admin = firebase;
