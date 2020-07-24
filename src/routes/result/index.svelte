156614dd14b5046e6557113c5862a61428780122
zakaria chahboun <zaki@localhost.localdomain>
2020-07-24 23:14:20 +0100

    fix result progressbar (css)

--- src/routes/result/index.svelte
+++ src/routes/result/index.svelte
@@ -1,9 +1,13 @@
+<script>
+  export let progress = 20;
+  export let neededToPass = 80;
+</script>
+
 <div class="text-component padding-md margin-auto margin-top-md max-width-md">
   <!-- Change the Style to User Score to change the color -->
   <div
-    class="progress-bar progress-bar--color-update js-progress-bar flex
-    flex-column items-center"
-    style="--progress-bar-color-1: 79%;"
+    class="progress-bar progress-bar--color-update flex flex-column items-center
+    js-progress-bar progress-bar--fill-color-1 progress-bar--init"
     data-animation="on"
     data-duration="2000">
     <div class="progress-bar__bg " aria-hidden="true">
@@ -11,21 +15,21 @@
       <div class="progress-bar__pass">
         Needed to pass
         <br />
-        80%
+        {neededToPass}%
       </div>
       <!-- Change the Style to User Score to change the value -->
-      <div class="progress-bar__fill" style="width: 80%;">
+      <div class="progress-bar__fill" style="width: {progress}%;">
         <div class="progress-bar__score">
           You Scored
           <br />
           <span class="progress-bar__value margin-bottom-xs" aria-hidden="true">
             <br />
-            /span>
+            50%
           </span>
         </div>
         <p class="sr-only" aria-live="polite" aria-atomic="true">
           Progress value is
-          <span class="js-progress-bar__aria-value" />
+          <span class="js-progress-bar__aria-value">{progress}%</span>
         </p>
       </div>
     </div>
