26bcea57b49b16280ef5b0e78486b457b25bd2be
zakaria chahboun <zaki@localhost.localdomain>
2020-08-29 14:35:21 +0100

    add firebase config for the web 'client'

---
+++ src/firebase-web-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCmEgGH3X-eKHVKU48tAJ2mWTIcFrbpjQE",
+    authDomain: "quiz-e9e49.firebaseapp.com",
+    databaseURL: "https://quiz-e9e49.firebaseio.com",
+    projectId: "quiz-e9e49",
+    storageBucket: "quiz-e9e49.appspot.com",
+    messagingSenderId: "374320758324",
+    appId: "1:374320758324:web:caa79e93fe37be198f9461"
+};
+
+export {
+    firebaseConfig
+};
