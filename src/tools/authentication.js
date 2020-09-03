38de9e110f84a8faaf035eefb0e37d9c40c7041a
zakaria chahboun <zaki@localhost.localdomain>
2020-09-03 15:43:35 +0100

    authentication: two way (JWT and Session)

--- src/tools/authentication.js
+++ src/tools/authentication.js
@@ -10,7 +10,12 @@ import {
     auth
 } from "../firebase-admin.js";
 
-// To extract authentication token from client request: this is a (polka or express) middleware 😉
+import {
+    StatusTypes
+} from "./status";
+import cookie from "cookie";
+
+// JWT: To extract authentication token from client request: this is a (polka or express) middleware 😉
 const getAuthToken = (req, res, next) => {
     if (
         req.headers.authorization &&
@@ -23,8 +28,8 @@ const getAuthToken = (req, res, next) => {
     next();
 };
 
-// To decode the token an retrive the user data 🤴
-export const checkAuthenticated = async (req, res, next) => {
+// JWT: To decode the token an retrive the user data 🤴
+export const JWTAuthentication = async (req, res, next) => {
     getAuthToken(req, res, async () => {
         try {
             const {
@@ -40,7 +45,44 @@ export const checkAuthenticated = async (req, res, next) => {
             req.user = userInfo;
             return next();
         } catch (error) {
-            easyResponse(res, null, true, error.code);
+            easyResponse(res, null, null, true, error.code);
         }
     });
 };
+
+// SESSION: To verify the firebase seesion cookie 🤴
+export const SessionAuthentication = async (req, res, next) => {
+    const sessionCookie = req.cookies.session || '';
+    /* Verify the session cookie. 
+       In this case an additional check is added to detect if the user's Firebase session was revoked, user deleted/disabled, etc.
+    */
+    auth.verifySessionCookie(
+            sessionCookie, true /** checkRevoked */ )
+        .then((decodedClaims) => {
+            req.user = {
+                uid: decodedClaims.uid,
+                email: decodedClaims.email,
+                name: decodedClaims.name || "",
+                avatar: decodedClaims.picture || "",
+                phoneNumber: decodedClaims.phone_number || "",
+                loginTime: Unix_timestamp(decodedClaims.auth_time),
+                isAnonymous: decodedClaims.isAnonymous || false,
+                emailVerified: decodedClaims.email_verified,
+            }
+            next();
+        })
+        .catch(error => {
+            // Session cookie is unavailable or invalid. Force user to login.
+            //easyResponse(res, null, null, true, StatusTypes.Login_Is_Required.code);
+            next()
+        });
+};
+
+// To Convert Unix Seconds to Normal HH:MM:SS time
+function Unix_timestamp(t) {
+    var dt = new Date(t * 1000);
+    var hr = dt.getHours();
+    var m = "0" + dt.getMinutes();
+    var s = "0" + dt.getSeconds();
+    return hr + ':' + m.substr(-2) + ':' + s.substr(-2);
+}
