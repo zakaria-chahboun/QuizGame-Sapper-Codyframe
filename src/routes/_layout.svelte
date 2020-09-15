0f84bc648e295c0e9824aedd5779c91b70ed5e6a
zakaria chahboun <zaki@localhost.localdomain>
2020-09-15 21:48:55 +0100

    one navbar in the holl app! again 🔥

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,6 +1,13 @@
 <script>
-  //export let segment;
-  // import Navbar from "../components/Navbar.svelte";
+  export let segment;
+  import Navbar from "../components/Navbar.svelte";
+  import { onMount } from "svelte";
+
+  let codyFrameScripts = "";
+  onMount(async () => {
+    // ---- To mount the CodyFrame scripts ----
+    codyFrameScripts = "codyframe/scripts.js";
+  });
 </script>
 
 <style lang="scss" global>
@@ -8,4 +15,11 @@
   @import "./codyframe/assets/css/style.scss";
 </style>
 
+<svelte:head>
+  <script defer src={codyFrameScripts}>
+
+  </script>
+</svelte:head>
+
+<Navbar {segment} />
 <slot />
