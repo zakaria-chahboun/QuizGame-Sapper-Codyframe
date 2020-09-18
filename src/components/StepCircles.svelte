d9c19176c5e376fc934f4d8e2d42042c2666c12d
zakaria chahboun <zaki@localhost.localdomain>
2020-09-18 02:28:20 +0100

    ui/ux: add loading animation to step circles 🔥

--- src/components/StepCircles.svelte
+++ src/components/StepCircles.svelte
@@ -1,6 +1,9 @@
 <script>
   import { goto } from "@sapper/app";
 
+  // Ui/Ux : loading data >> for tell the user to wait
+  let wait = false;
+
   // data format:
   export let data = [
     { type: "", url: "#", index: 1 },
@@ -10,8 +13,34 @@
   ];
 </script>
 
+<style>
+  .is-striped {
+    background-image: repeating-linear-gradient(
+      -45deg,
+      #e0efff,
+      #e0efff 0.5rem,
+      #1f87ff 0.5rem,
+      #1f87ff 1rem
+    );
+    background-size: 200% 200%;
+    animation: progress 7s linear infinite;
+  }
+
+  @keyframes progress {
+    100% {
+      background-position: 100% 100%;
+    }
+  }
+</style>
+
 {#each data as { type, index, url }}
-  <div class="progress-cell {type}" on:click={() => goto(url)}>
+  <div
+    class="progress-cell {wait ? "is-striped" : type}"
+    on:click={async () => {
+      wait = !wait;
+      await goto(url);
+      wait = !wait;
+    }}>
     <p>{index}</p>
   </div>
 {/each}
