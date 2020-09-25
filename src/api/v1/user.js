336239ade4d34f08f44219e95fa48281f1c1e24d
zakaria chahboun <zaki@localhost.localdomain>
2020-09-25 20:18:49 +0100

    api 🔥 add clean user api + hundling errors

--- src/api/v1/user.js
+++ src/api/v1/user.js
@@ -15,41 +15,137 @@ import {
     auth
 } from "../../firebase-admin";
 
+import fetch from 'node-fetch';
+
+/* 
+    this is the production host name
+    exapmle 1: https://api.myapp.com
+    exapmle 2: https://www.myapp.com
+*/
+import {
+    hostname
+} from '../hostname.json';
+
 const {
     PORT,
     NODE_ENV,
 } = process.env;
 const dev = NODE_ENV === 'development';
 
+const coreApi = dev ? `http://localhost:${PORT}/api/v1/core` : `${hostname}/api/v1/core`;
+
 api_v1_user_router
-    .get('/user/test/:test_id', async (req, res) => {
-
-        // First of all: check the existence of user
-        if (!req.user) {
-            return easyResponse(res, null, true, StatusTypes.Login_Is_Required.code);
-        }
-        const {
-            test_id
-        } = req.params;
-
+    // -- get all tests data from db --
+    .get('/user/tests/:id?', async (req, res) => {
         try {
-            let testDoc = await firestore
-                .collection("tests")
-                .doc(test_id)
-                .get();
+            // variables
+            let testData = {}; // The final clean result (maybe array of objects if tests, maybe one object if one test)
+            let apiUrl = '';
+            const testID = req.params.id;
 
-            if (!testDoc.exists) {
-                return easyResponse(res, null, true, StatusTypes.NO_DATA.code);
+            // [option 1] Get core test by ID
+            // [option 2] Get all core tests
+            testID ? apiUrl = `${coreApi}/tests/${testID}` : apiUrl = `${coreApi}/tests`;
+
+            // ------------------------- Step 1 -------------------------
+
+            // Get test or tests
+            const snapshot = await fetch(apiUrl);
+            let result = await snapshot.json();
+            if (result.status.isError) return easyResponse(res, null, true, result.status.code, result.status.message);
+            testData = result.data;
+
+            // ------------------------- Step 2 -------------------------
+
+            // User Variables 🤦‍♂
+            let userProgress = [];
+            // Check if user logged in >> then get his progress
+            if (req.user) {
+                let userID = req.user.uid;
+                const userDoc = await firestore
+                    .collection("users").doc(userID)
+                    .get();
+
+                // Check if the User alredy exist and has the 'lastTest' field 🐶 in db
+                if (userDoc.exists && userDoc.data().lastTest) {
+                    const userData = await firestore
+                        .collection("users").doc(userID).collection('userProgress')
+                        .get();
+
+                    // array of maps (js objects) 🤠
+                    userProgress = userData.docs.map(doc => {
+                        let data = doc.data();
+                        data.id = doc.id;
+                        return data;
+                    });
+
+                    // const userLastTest = await userDoc.data().lastTest.get();
+                    // console.log(userLastTest.data());
+                }
             }
 
-            easyResponse(res, {
-                a: 1,
-                b: 2
-            }, );
+            // ------------------------- Step 3 -------------------------
 
+            // [option 1]: return data of test by id
+            if (testID) {
+                testData.stepValue = 0; // by default: no step 🙄!
+                testData.isCompleted = false; // by default: no user play 🙄!
+                testData.lastQuestion = 0; // by default: no user play 🙄!
 
+                // if the user logged and exist >> update the progress 🦊
+                for (const e of userProgress) {
+                    if (e.id == testID) {
+                        testData.stepValue = e.stepValue;
+                        testData.isCompleted = e.isCompleted;
+                        testData.lastQuestion = e.lastQuestion;
+                        break;
+                    }
+                }
+
+                const snapshot = await fetch(`${coreApi}/questions/test/${testData.id}`);
+                const result = await snapshot.json();
+                if (result.status.isError) return easyResponse(res, null, true, result.status.code, result.status.message);
+
+                const max = result.data.length;
+                testData.maxSteps = max;
+                return easyResponse(res, testData);
+            }
+            // [option 2]: return data of all tests
+            else {
+                let tests = [];
+                for (let doc of testData) {
+                    doc.stepValue = 0; // by default: no step 🙄!
+                    doc.isCompleted = false; // by default: no user play 🙄!
+                    doc.lastQuestion = 0; // by default: no user play 🙄!
+
+                    // if the user logged and exist >> update the progress 🦊
+                    for (const e of userProgress) {
+                        if (e.id == doc.id) {
+                            doc.stepValue = e.stepValue;
+                            doc.isCompleted = e.isCompleted;
+                            doc.lastQuestion = e.lastQuestion;
+                            break;
+                        }
+                    }
+
+                    // finally add 'max steps' for the current test 😉
+                    // this is useful to not increase question index more than it have (out of range) 👌
+                    const snapshot = await fetch(`${coreApi}/questions/test/${doc.id}`);
+                    const result = await snapshot.json();
+                    if (result.status.isError) return easyResponse(res, null, true, result.status.code, result.status.message);
+
+                    doc.maxSteps = result.data.length;
+                    tests.push(doc);
+                }
+
+                // Promise.all is useful if you have a "async/await" inside the "map" function of your array
+                //const tests = await Promise.all(_tests_);
+                return easyResponse(res, tests);
+            }
         } catch (error) {
-            return easyResponse(res, null, true, error.code);
+            console.log(error);
+
+            easyResponse(res, null, true, error.code);
         }
     })
 
