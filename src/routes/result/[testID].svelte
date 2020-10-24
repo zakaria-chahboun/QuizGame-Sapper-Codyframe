63caeacf57bf855eb6f2af714da8cd53c061d04d
zakaria-chahboun <zakaria.chahboun.1996@gmail.com>
2020-10-24 12:59:24 +0100

    Update Result page

--- src/routes/result/[testID].svelte
+++ src/routes/result/[testID].svelte
@@ -1,59 +1,83 @@
 <script context="module">
+  import { StatusTypes } from "../../tools/status.js";
   export async function preload(page, session) {
     let { user } = session;
     let { testID } = page.params;
-    let isCompleted;
+    let isError = false;
+    let message = "";
 
-    if (!user) this.redirect(302, "/");
+    if (!user || user.isAnonymous) return this.redirect(302, "/login");
 
-    const result = await this.fetch(`api/v1/user/tests/${testID}`);
+    const result = await this.fetch(`api/v1/user/result/${testID}`);
     const testData = await result.json();
 
-    if (testData.status.isError)
-      return this.error(result.status, testData.status.message);
+    // check errors
+    if (testData.status.isError) {
+      // No User or Anonymous?
+      if (testData.status.code == StatusTypes.Login_Is_Required.code)
+        return this.redirect(302, "/signup");
+    }
 
+    // No Completed Test = No Result baby 🍪!
     if (!testData.data.isCompleted)
-      return this.redirect(
-        302,
-        `/test/${testID}/${testData.data.lastQuestion}`
-      );
+      return this.redirect(302, `/test/${testID}/1`);
+
+    return { ...testData.data };
   }
 </script>
 
 <script>
-  export let progress = 20;
-  export let neededToPass = 80;
+  export let isError;
+  export let message;
+
+  export let progress;
+  export let needToPass;
+
+  export let test;
+  export let testTitle;
+  export let testSubtitle;
 </script>
 
-<div class="text-component padding-md margin-auto margin-top-md max-width-md">
-  <!-- Change the Style to User Score to change the color -->
-  <div
-    class="progress-bar progress-bar--color-update flex flex-column items-center
-    js-progress-bar progress-bar--fill-color-1 progress-bar--init"
-    data-animation="on"
-    data-duration="2000">
-    <div class="progress-bar__bg " aria-hidden="true">
-      <!-- Pass Success Score for the Test -->
-      <div class="progress-bar__pass">
-        Needed to pass
-        <br />
-        {neededToPass}%
-      </div>
-      <!-- Change the Style to User Score to change the value -->
-      <div class="progress-bar__fill" style="width: {progress}%;">
-        <div class="progress-bar__score">
-          You Scored
+{#if isError}
+  {message}
+{:else}
+  <div class="text-component padding-md margin-auto margin-top-md max-width-md">
+    <!-- Change the Style to User Score to change the color -->
+    <div
+      class="progress-bar progress-bar--color-update flex flex-column
+      items-center js-progress-bar progress-bar--fill-color-1 progress-bar--init"
+      data-animation="on"
+      data-duration="2000">
+      <div class="progress-bar__bg " aria-hidden="true">
+        <!-- Pass Success Score for the Test -->
+        <div class="progress-bar__pass">
+          Needed to pass
           <br />
-          <span class="progress-bar__value margin-bottom-xs" aria-hidden="true">
-            <br />
-            50%
-          </span>
+          {needToPass}%
         </div>
-        <p class="sr-only" aria-live="polite" aria-atomic="true">
-          Progress value is
-          <span class="js-progress-bar__aria-value">{progress}%</span>
-        </p>
+        <!-- Change the Style to User Score to change the value -->
+        <div class="progress-bar__fill" style="width: {progress}%;">
+          <div class="progress-bar__score">
+            You Scored
+            <br />
+            <span
+              class="progress-bar__value margin-bottom-xs"
+              aria-hidden="true">
+              <br />
+              {progress}%
+            </span>
+          </div>
+          <p class="sr-only" aria-live="polite" aria-atomic="true">
+            Progress value is
+            <span class="js-progress-bar__aria-value">{progress}%</span>
+          </p>
+        </div>
       </div>
     </div>
   </div>
-</div>
+  <!-- Test Title -->
+  <br />
+  {testTitle}
+  <br />
+  {testSubtitle}
+{/if}
