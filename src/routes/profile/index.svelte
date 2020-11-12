db18a618f37467269f6373e681a2b47f0699e385
zakaria chahboun <zaki@pop-os.localdomain>
2020-11-12 16:04:59 +0100

    Add Profile page

---
+++ src/routes/profile/index.svelte
@@ -0,0 +1,25 @@
+<script context="module">
+  export async function preload(page, session) {
+    let { user } = session;
+    //console.log(user);
+    if (user && user.isAnonymous) this.redirect(302, "/");
+    return { user };
+  }
+</script>
+
+<script>
+  import { FirebaseProviders } from "../../tools/cool.js";
+  export let user;
+</script>
+
+<pre>
+    {user.name} <br />
+    
+    {user.email} <br />
+    {#if user.provider == FirebaseProviders.google}
+    
+    You are login with google account!
+
+  {/if}
+
+</pre>
