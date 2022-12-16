755999a20a002ac83accb18ec1ccd93c0955d226
zakaria chahboun <zakaria.chahboun.1996@gmail.com>
2022-12-16 21:49:16 +0100

    Update firebase-web-config.js

--- src/firebase-web-config.js
+++ src/firebase-web-config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBtCMQ7J1NYbV1aigdIASBuKnXBGltO5ko",
-    authDomain: "life-in-the-uk.firebaseapp.com",
-    databaseURL: "https://life-in-the-uk.firebaseio.com",
-    projectId: "life-in-the-uk",
-    storageBucket: "life-in-the-uk.appspot.com",
-    messagingSenderId: "422779075426",
-    appId: "1:422779075426:web:9a74464ec01c6f7cb40a3b"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 export {
